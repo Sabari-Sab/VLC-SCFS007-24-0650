a66c46dc139847d950a222a04cc8508dcbd89cfa
jiakslim <limjiakhee@gmail.com>
2025-01-04 16:01:37 +0800

    Fixed MyUserDetails.java and AccountController.java problems that resulted in the application not being able to start. Changed getUsername() function from return user.getUsername(); to return user.getUserName();

--- bankbackend/src/main/java/com/ntuc/bankbackend/controllers/AccountController.java
+++ bankbackend/src/main/java/com/ntuc/bankbackend/controllers/AccountController.java
@@ -3,7 +3,6 @@ package com.ntuc.bankbackend.controllers;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
--- bankbackend/src/main/java/com/ntuc/bankbackend/security/MyUserDetails.java
+++ bankbackend/src/main/java/com/ntuc/bankbackend/security/MyUserDetails.java
@@ -3,7 +3,6 @@ package com.ntuc.bankbackend.security;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -38,7 +37,7 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public String getUsername() {
-        return user.getUsername();
+        return user.getUserName();
     }
     
 }
