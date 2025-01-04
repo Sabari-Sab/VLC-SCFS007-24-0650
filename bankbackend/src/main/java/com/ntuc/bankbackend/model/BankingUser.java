80f0daec9ce4164b8a4f29d7db53161bab4ef411
SabariNathan <sabarinathan9422@gmail.com>
2025-01-04 15:48:28 +0800

    change fieldname in BankingUser userName

--- bankbackend/src/main/java/com/ntuc/bankbackend/model/BankingUser.java
+++ bankbackend/src/main/java/com/ntuc/bankbackend/model/BankingUser.java
@@ -23,7 +23,7 @@ public class BankingUser {
     @Column(name="user_id")
     private Long id;
     @Column(name="user_name")
-    private String username;
+    private String userName;
     private String password;
     private String name;
     private String email;
