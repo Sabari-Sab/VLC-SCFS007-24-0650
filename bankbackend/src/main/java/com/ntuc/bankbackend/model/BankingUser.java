2962cf6daf58f73d426db08388ca3216a403be80
SabariNathan <sabarinathan9422@gmail.com>
2025-01-04 14:21:35 +0800

    Changes List Roles to role

--- bankbackend/src/main/java/com/ntuc/bankbackend/model/BankingUser.java
+++ bankbackend/src/main/java/com/ntuc/bankbackend/model/BankingUser.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,9 +30,9 @@ public class BankingUser {
     private String password;
     private String name;
     private String email;
-    @OneToMany(cascade= CascadeType.ALL)
+    @OneToOne(cascade= CascadeType.ALL)
     @JoinColumn(name="role_id")
-    private List<Roles> roles;
+    private Roles role;
 
 
 }
